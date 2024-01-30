bc75dbc625e8f56c966fbce0ac68b88cad10fdf6
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-01-30 18:58:30 +0200

    update

---
+++ EventsAndMuticastDelegates/EventsAndMuticastDelegates/AudioSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndMuticastDelegates
+{
+    internal class AudioSystem
+    {
+        public AudioSystem()
+        {
+            GameEventManager.OnGameStart += StartGame;
+            GameEventManager.OnGameOver += GameOver;
+        }
+
+        private void StartGame()
+        {
+            Console.WriteLine("Audio System Started");
+            Console.WriteLine("Playing Audio...");
+        }
+
+        private void GameOver()
+        {
+            Console.WriteLine("Audio System Stopped");
+        }
+    }
+}
---
+++ EventsAndMuticastDelegates/EventsAndMuticastDelegates/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EventsAndMuticastDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AudioSystem audioSystem = new AudioSystem();
+            RenderingEngine renderingEngine = new RenderingEngine();
+            
+            Player player1 = new Player("Steelcow");
+            Player player2 = new Player("DoggoSilva");
+
+            GameEventManager.TriggerGameStart();
+
+            Console.WriteLine("Game is running... Press any key to end the game");
+            Console.Read();
+
+            GameEventManager.TriggerGameOver();
+
+        }
+    }
+}
